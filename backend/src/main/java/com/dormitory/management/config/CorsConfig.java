3d7a1c5538f0aed5c85b811da9534688d1325251
thanhlthe194454 <trungthanh1612005@gmail.com>
2026-08-10 15:11:12 +0700

    Add Dockerfile

--- backend/src/main/java/com/dormitory/management/config/CorsConfig.java
+++ backend/src/main/java/com/dormitory/management/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173")
+                .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://quanliktxcpt.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
